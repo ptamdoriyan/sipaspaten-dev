a28de7f5f81222966874f86b5a200e4f31fad54f
riyanboyuhu <riyanboyuhu@gmail.com>
2023-09-16 12:46:46 +0800

    Buat Perdana

---
+++ app/Database/Migrations/2023-09-16-043407_TableUserStart.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+use CodeIgniter\Database\RawSql;
+
+class TableUserStart extends Migration
+{
+    public function up()
+    {
+        //
+
+        $this->forge->addField(
+            [
+                'id_user'          => [
+                    'type'           => 'INT',
+                    'constraint'     => 5,
+                    'unsigned'       => true,
+                    'auto_increment' => true
+                ],
+                'id_uniq'       => [
+                    'type'           => 'VARCHAR',
+                    'constraint'     => '255'
+                ],
+                'nama'       => [
+                    'type'           => 'VARCHAR',
+                    'constraint'     => '128'
+                ],
+                'whatsapp'       => [
+                    'type'           => 'VARCHAR',
+                    'constraint'     => '15'
+                ],
+                'email'       => [
+                    'type'           => 'VARCHAR',
+                    'constraint'     => '128'
+                ],
+                'password'       => [
+                    'type'           => 'VARCHAR',
+                    'constraint'     => '15'
+                ],
+                'role_id'       => [
+                    'type'           => 'INT',
+                    'constraint'     => '11'
+                ],
+                'is_active'       => [
+                    'type'           => 'INT',
+                    'constraint'     => '1'
+                ],
+                'date_creted' => [
+                    'type'    => 'TIMESTAMP',
+                    'default' => new RawSql('CURRENT_TIMESTAMP'),
+                ],
+
+            ]
+        );
+
+        //membuat primary key
+        $this->forge->addKey('id_user', TRUE);
+        //membuat tabel
+        $this->forge->createTable('users', true);
+    }
+
+    public function down()
+    {
+        //menghapus tabel
+        $this->forge->dropTable('users');
+    }
+}
