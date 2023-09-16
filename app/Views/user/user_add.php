992352deb3e59ee2901a1352885ef7bf9b00f7f4
riyanboyuhu <riyanboyuhu@gmail.com>
2023-09-16 15:08:48 +0800

    perbaiki form upload putusan

--- app/Views/user/user_add.php
+++ app/Views/user/user_add.php
@@ -11,28 +11,28 @@
             <form class="row g-3" method="POST">
                 <div class="col-md-6">
                     <label for="inputEmail4" class="form-label">Nomor Putusan</label>
-                    <input type="text" class="form-control" id="inputNomorPutusan" name="nomorputusan" required>
+                    <input type="text" class="form-control" id="inputNomorPutusan" name="nomorputusan" required autofocus>
                 </div>
                 <div class="col-md-6">
                     <label for="formFileSm" class="form-label">Upload Putusan</label>
                     <input class="form-control form-control-sm" id="formFileSm" type="file" name="uploadputusan">
                 </div>
-        </div>
-        <div class="col-12">
-            <div class="form-check">
-                <input class="form-check-input" type="checkbox" id="gridCheck" required>
-                <label class="form-check-label" for="gridCheck">
-                    Dengan ini saya Yakin bahwa data yang dikirimkan sudah benar !
-                </label>
-            </div>
-        </div>
-        <div class="col-12">
-            <button type="submit" class="btn btn-primary">Upload</button>
-        </div>
-        </form>
-    </div>
 
-</div>
+                <div class="col-12">
+                    <div class="form-check">
+                        <input class="form-check-input" type="checkbox" id="gridCheck" required>
+                        <label class="form-check-label" for="gridCheck">
+                            Dengan ini saya Yakin bahwa data yang dikirimkan sudah benar !
+                        </label>
+                    </div>
+                </div>
+                <div class="col-12">
+                    <button type="submit" class="btn btn-primary">Upload</button>
+                </div>
+            </form>
+        </div>
+
+    </div>
 
 </div>
 
