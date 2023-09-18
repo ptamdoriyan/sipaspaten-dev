87d1a1378ebf5a6954307219f81a1ef61c82cd44
riyanboyuhu <riyanboyuhu@gmail.com>
2023-09-18 09:47:14 +0800

    membuat migration Audit Trail dan Model Audit Trail

---
+++ app/Database/Migrations/2023-09-18-013924_CreateAuditTrail.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+use CodeIgniter\Database\RawSql;
+
+class CreateAuditTrail extends Migration
+{
+    public function up()
+    {
+        //inisiasi tabel
+        $this->forge->addField(
+            [
+                'id_log'          => [
+                    'type'           => 'INT',
+                    'constraint'     => 5,
+                    'unsigned'       => true,
+                    'auto_increment' => true
+                ],
+                'id_uniq'          => [
+                    'type'           => 'INT',
+                    'constraint'     => 5
+                ],
+                'action'       => [
+                    'type'           => 'VARCHAR',
+                    'constraint'     => '255'
+                ],
+                'date' => [
+                    'type'    => 'TIMESTAMP',
+                    'default' => new RawSql('CURRENT_TIMESTAMP'),
+                ],
+            ]
+        );
+
+        $this->forge->addKey('id_log', true);
+        $this->forge->createTable('logs', true);
+    }
+
+    public function down()
+    {
+        //
+        $this->forge->dropTable('logs');
+    }
+}
