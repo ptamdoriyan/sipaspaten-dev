73e350c3a3653ac4a3ffc095dd42a047855ccac0
riyanboyuhu <riyanboyuhu@gmail.com>
2023-09-23 12:29:16 +0800

    menambahkan triger ketika berita acara dihapus

--- app/Database/Migrations/2023-09-22-141625_CreateTigerUpdateStatus.php
+++ app/Database/Migrations/2023-09-22-141625_CreateTigerUpdateStatus.php
@@ -8,11 +8,16 @@ class CreateTigerUpdateStatus extends Migration
 {
     public function up()
     {
-        //
+        //triger create berita acara
         $db = db_connect();
-        $db->query("CREATE TRIGGER `update_status` AFTER INSERT ON `berita_acara`
+        $db->query("CREATE TRIGGER `insert_berita_acara` AFTER INSERT ON `berita_acara`
         FOR EACH ROW UPDATE penetapan SET penetapan.status = 2
        WHERE id_penetapan = NEW.id_penetapan");
+
+        //triger delete berita acara
+        $db->query("CREATE TRIGGER `delete_berita_acara` AFTER DELETE ON `berita_acara`
+        FOR EACH ROW UPDATE penetapan SET penetapan.status = 1
+       WHERE id_penetapan = OLD.id_penetapan");
     }
 
     public function down()
